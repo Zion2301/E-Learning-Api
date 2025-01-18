f57439146383a25359d8bcc3280e0ba71c859b84
Zion <omogbemezion@gmail.com>
2025-01-18 16:24:36 +0100

    idk

--- src/Route/googleRouter.ts
+++
@@ -1,9 +0,0 @@
-import { googlesignup } from "../middleware/googleMiddleware";
-import express, { NextFunction, Response } from "express"
-import { CustomRequest } from "../middleware/googleMiddleware";
-
-const googleRoute = express.Router();
-
-googleRoute.post("/google-signup", googlesignup);
-
-export default googleRoute;
