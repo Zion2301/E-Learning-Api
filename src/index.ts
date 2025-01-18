3a2920239b3714bcb839fe20cc5f89b09dcebfda
Zion <omogbemezion@gmail.com>
2025-01-18 19:43:24 +0100

    idk

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,48 @@
-import express  from "express";
-import cors from "cors";
-import dotenv from "dotenv";
-import userRouter from "./Route/userRoute";
-import courseRouter from "./Route/courseRoute"
-import authRouter from "./Route/authRouter";
-import { errorHandler } from "./error/errorHandler";
+// import express  from "express";
+// import cors from "cors";
+// import dotenv from "dotenv";
+// import userRouter from "./Route/userRoute";
+// import courseRouter from "./Route/courseRoute"
+// import authRouter from "./Route/authRouter";
+// import { errorHandler } from "./error/errorHandler";
 
 
 
-dotenv.config();
+// dotenv.config();
 
-const portEnv = process.env.PORT;
+// const portEnv = process.env.PORT;
 
-if (!portEnv) {
-    console.error("Error: PORT is not defined in .env file");
-    process.exit(1);
-}
+// if (!portEnv) {
+//     console.error("Error: PORT is not defined in .env file");
+//     process.exit(1);
+// }
 
-const PORT: number = parseInt(portEnv,10)
-if (isNaN(PORT)) {
-    console.error("Error: PORT is not a number in .env file")
-    process.exit(1)
-}
+// const PORT: number = parseInt(portEnv,10)
+// if (isNaN(PORT)) {
+//     console.error("Error: PORT is not a number in .env file")
+//     process.exit(1)
+// }
 
-const app = express();
-const corsOption = {
-    origin:
-    "*",
-    credentials: true,
-    allowedHeaders: "*",
-    methods:"GET, HEAD, PUT, PATCH, POST, DELETE"
-};
+// const app = express();
+// const corsOption = {
+//     origin:
+//     "*",
+//     credentials: true,
+//     allowedHeaders: "*",
+//     methods:"GET, HEAD, PUT, PATCH, POST, DELETE"
+// };
 
-app.use(cors(corsOption));
+// app.use(cors(corsOption));
 
-app.use(express.json());
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/course", courseRouter)
-app.use("/api/v1/login", authRouter)
-app.use(errorHandler)
+// app.use(express.json());
+// app.use("/api/v1/users", userRouter)
+// app.use("/api/v1/course", courseRouter)
+// app.use("/api/v1/login", authRouter)
+// app.use(errorHandler)
 
-app.listen(PORT, ()=> {
-    console.log(`Server is running on port ${PORT}`)
-});
+// app.listen(PORT, ()=> {
+//     console.log(`Server is running on port ${PORT}`)
+// });
 
 
 
--- src/middleware/passprtConfig.ts
+++ src/middleware/passprtConfig.ts
@@ -1,3 +1,76 @@
-import passport from "passport"
+import passport, { Profile } from "passport"
 import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 import { Strategy as GitHubStrategy } from 'passport-github2';
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+passport.use(new GoogleStrategy (  //defines the google oAuth strategy for passport
+    {
+    clientID: process.env.GOOGLE_CLIENT_ID!,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL  //necessary values to authenticate with google
+},
+  async (accessToken, refreshToken, profile, done) => {  //these are the paramtres provided by google during authentication
+      try {
+         const user = await prisma.user.upsert({  //check if a usr exists by their google id
+             where: {googleId: profile.id},
+             update: {},
+             create: {
+                googleId: profile.id,
+                email: profile.emails?.[0]?.value ?? '',
+                firstName: profile.name?.givenName ?? '',
+                lastName: profile.name?.familyName ?? '',
+                password: profile.provider,
+             },
+         });
+         return done(null, user)
+      } catch (err) {
+        console.error('Error in Google OAuth:', err);
+      return done(err);
+      }
+  }
+));
+
+passport.use( new GitHubStrategy(
+{
+   clientID: process.env.GITHUB_CLIENT_ID!,
+   clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+   callbackURL: process.env.GITHUB_CALLBACK_URL!,
+},
+  async (accessToken: string, refreshToken:string, profile:Profile, done: any) => {
+       try {
+          const user = await prisma.user.upsert({
+            where: {githubId: profile.id},
+            update: {},
+            create: {
+                githubId: profile.id,
+          email: profile.emails?.[0]?.value ?? '',
+          firstName: profile.username ?? '',
+          password: profile.provider
+            }
+          })
+          return done(null, user)
+       } catch (err) {
+        console.error('Error in GitHub OAuth:', err);
+        return done(err, null);
+       }
+  }
+))
+
+passport.serializeUser((user: any, done) => {  //serializing user saves the users id in the session or token
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(async (id: number, done) => {  //retrieves the id when needed
+    try {
+      const user = await prisma.user.findUnique({
+        where: { id },
+      });
+      done(null, user);
+    } catch (err) {
+      done(err, null);
+    }
+  });
+  
+
+export default passport
